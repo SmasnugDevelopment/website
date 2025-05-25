2e8eaec270d97be0b9697c4f47096cf9b79ca39d
Ingo Wolf <me@ingowolf.au>
2025-05-25 13:48:37 +1000

    style privacy page

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,12 +1,16 @@
+import { Section } from "lucide-react";
+
 export default function PrivacyPage() {
   return (
-    <div>
-      <h1>Privacy Policy</h1>
-      <p>
-        At our website, we are committed to protecting your privacy and ensuring
-        the security of your personal information. This Privacy Policy outlines
-        how we collect, use, and disclose your information.
-      </p>
-    </div>
+    <section className="p-4 flex justify-center">
+      <div className="prose-invert prose container">
+        <h1>Privacy Policy</h1>
+        <p>
+          At our website, we are committed to protecting your privacy and
+          ensuring the security of your personal information. This Privacy
+          Policy outlines how we collect, use, and disclose your information.
+        </p>
+      </div>
+    </section>
   );
 }
