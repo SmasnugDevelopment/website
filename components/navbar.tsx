051fbf14704b4789ad12224d1c7cb11df80fd64a
Ingo Wolf <me@ingowolf.au>
2025-05-25 10:01:43 +1000

    fix type checking issue

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -249,7 +249,7 @@ const renderMobileMenuItem = (item: MenuItem) => {
   return (
     <Link
       key={item.title}
-      href={item.url}
+      href={item.url || "#"}
       className="text-md font-semibold"
       target={item.target}
     >
@@ -262,7 +262,7 @@ const SubMenuLink = ({ item }: { item: MenuItem }) => {
   return (
     <Link
       className="flex flex-row gap-4 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none hover:bg-muted hover:text-accent-foreground"
-      href={item.url}
+      href={item.url || "#"}
       target={item.target}
     >
       <div className="text-foreground">{item.icon}</div>
