1bbc1520e48edd9a8b59152b3ae872a37412c3b8
Ingo Wolf <me@ingowolf.au>
2025-05-26 10:22:05 +1000

    add cards

---
+++ components/cards.tsx
@@ -0,0 +1,55 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+interface CardItem {
+  name: string;
+  description: string;
+  links?: {
+    name: string;
+    url: string;
+    target?: string;
+    variant?:
+      | "outline"
+      | "link"
+      | "default"
+      | "destructive"
+      | "secondary"
+      | "ghost";
+  }[];
+}
+
+interface CardsProps {
+  stuffs: CardItem[];
+}
+
+export default function Cards({ stuffs }: CardsProps) {
+  return (
+    <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
+      {stuffs.map((thing, index) => (
+        <Card key={index}>
+          <CardHeader>
+            <CardTitle>{thing.name}</CardTitle>
+            <CardDescription>{thing.description}</CardDescription>
+          </CardHeader>
+          <CardFooter className="flex flex-row gap-1">
+            {thing.links?.map((link) => (
+              <Button key={link.name} variant={link.variant}>
+                <Link href={link.url} target={link.target}>
+                  {link.name}
+                </Link>
+              </Button>
+            ))}
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
