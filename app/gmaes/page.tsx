97f9a80ae137923cc269f5b9bbad8f1001f11a98
Ingo Wolf <me@ingowolf.au>
2025-05-26 10:09:08 +1000

    add gmaes apge

--- app/gmaes/page.tsx
+++ app/gmaes/page.tsx
@@ -1,7 +1,58 @@
-export default function GmaesPage() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button, type buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function StuffPage() {
+  const gmaes: {
+    name: string;
+    description: string;
+    links?: {
+      name: string;
+      url: string;
+      target?: string;
+      variant?:
+        | "outline"
+        | "link"
+        | "default"
+        | "destructive"
+        | "secondary"
+        | "ghost";
+    }[];
+  }[] = [];
+
   return (
-    <div>
-      <h1>Gmaes</h1>
-    </div>
+    <section className="flex justify-center p-4">
+      <div className="container flex flex-col gap-4">
+        <h1 className="text-4xl">Gmaes</h1>
+        <p>Mostly in Godot</p>
+        <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
+          Coming soon™
+          {gmaes.map((thing) => (
+            <Card>
+              <CardHeader>
+                <CardTitle>{thing.name}</CardTitle>
+                <CardDescription>{thing.description}</CardDescription>
+              </CardHeader>
+              <CardFooter className="flex flex-row gap-1">
+                {thing.links?.map((link) => (
+                  <Button key={link.name} variant={link.variant}>
+                    <Link href={link.url} target={link.target}>
+                      {link.name}
+                    </Link>
+                  </Button>
+                ))}
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
