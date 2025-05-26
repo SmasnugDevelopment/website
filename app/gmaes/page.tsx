417142206e3b0b5d191ffc06cd47558645878854
Ingo Wolf <me@ingowolf.au>
2025-05-26 10:26:13 +1000

    remove more unnesacarry code

--- app/gmaes/page.tsx
+++ app/gmaes/page.tsx
@@ -1,23 +1,6 @@
 import Cards from "@/components/cards";
 
 export default function StuffPage() {
-  const gmaes: {
-    name: string;
-    description: string;
-    links?: {
-      name: string;
-      url: string;
-      target?: string;
-      variant?:
-        | "outline"
-        | "link"
-        | "default"
-        | "destructive"
-        | "secondary"
-        | "ghost";
-    }[];
-  }[] = [];
-
   return (
     <section className="flex justify-center p-4">
       <div className="container flex flex-col gap-4">
