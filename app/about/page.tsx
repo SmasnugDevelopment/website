5ebff0872da88965876b2eedf0a27a4d0e5f8c7c
Ingo Wolf <me@ingowolf.au>
2025-05-26 10:18:04 +1000

    add about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import Link from "next/link";
+
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>We are a team of passionate developers...</p>
-    </div>
+    <section className="p-4 flex justify-center">
+      <div className="prose-lg container">
+        <h1>About Us</h1>
+        <p>We are a team of devs who like making random things.</p>
+      </div>
+    </section>
   );
 }
